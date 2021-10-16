4d0fda1d9cdc5401f36ef0d3237d3a11c03a9fca
Alejo <80418452+AlejoTorres2001@users.noreply.github.com>
2021-10-16 20:26:46 -0300

    test

--- 1_read.py
+++ 1_read.py
@@ -11,12 +11,4 @@ cv.waitKey(0)
 # Reading Videos
 capture = cv.VideoCapture("Videos/Spider-Man-3.mp4")  # Reference WEBCAM
 
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow('Video', frame)
-
-    if cv.waitKey(20) & 0xFF == ord('d'):
-        break
-
-capture.release()
-cv.destroyAllWindows()
+print("Hola!")
